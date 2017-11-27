ce531697ecf626dcccb446e184dd98199da8abac
Kathy Hudson <khitx@icloud.com>
2017-11-27 23:01:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-
-
-//log which key 'keydown' was pressed in the 'input' node
-//const input = document.querySelector('input')
-//input.addEventListener('keydown', function(e) {
-  //console.log(e.which)
-//})
-///
   document.body.addEventListener('keydown', onKeyDownHandler)
-
-
-// Keep track of index outside of the event handler.
   var index = 0;
 
-// This is the function that would be invoked by the event listener.
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-    if (key === alphabet[index]) {
+    if (key === code[index]) {
       index++;
 
-      if (index === alphabet.length) {
+      if (index === code.length) {
         alert("Hurray!");
         index = 0;
       }
